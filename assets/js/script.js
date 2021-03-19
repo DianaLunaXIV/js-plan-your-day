54c78feba724d86c64c11b4a38e4a94afc449512
Brent B <brentbaughan1@gmail.com>
2021-03-18 20:29:24 -0400

    refactor with included index.html/style.css upon realizing they exist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-//
+function loadTimes(){
+    document.getElementById('timeDisplay').innerHTML = moment().format('h:mm:ss a');
+    document.getElementById('dateDisplay').innerHTML = moment().format('MMMM Do YYYY');
+};
 const currentDate = setInterval(loadTimes, 1000);
 // document.getElementById('dateDisplay').innerHTML = currentDate.format('MMMM Do YYYY');
 // document.getElementById('timeDisplay').innerHTML = currentDate.format('h:mm a');
 
-function loadTimes(){
-        document.getElementById('timeDisplay').innerHTML = moment().format('h:mm:ss a');
-        document.getElementById('dateDisplay').innerHTML = moment().format('MMMM Do YYYY');
+
+
+
+
+const dailyPlans = {9: "", 10: "", 11: "", 12: "", 1: "", 2: "", 3: "", 4: "", 5: ""};
+
+
+
+
+
+
+function createKey() {
+    const key = moment().format('MM DD YYYY');
+    return key;
 };
 
 
 
-
-
-
-
-
-
-
-function getKey() {
-
-};
-
-function saveTime(input){
-    localStorage.setItem()
+function saveTimes(){
+    
+    localStorage.setItem(createKey(), dailyPlans)
 };
