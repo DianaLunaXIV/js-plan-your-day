572eac1a9cf94a5bda01d18670abfa97ef8ae292
Brent B <brentbaughan1@gmail.com>
2021-03-20 13:45:29 -0400

    styled image and jumbotron bars a bit more

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
+//Load times to be displayed in jumbotron
 function loadTimes(){
     document.getElementById('timeDisplay').innerHTML = moment().format('h:mm:ss a');
     document.getElementById('dateDisplay').innerHTML = moment().format('dddd, MMMM Do YYYY');
-    };
+};
+//Update times every second
 const currentDate = setInterval(loadTimes, 1000);
-    // document.getElementById('dateDisplay').innerHTML = currentDate.format('MMMM Do YYYY');
-    // document.getElementById('timeDisplay').innerHTML = currentDate.format('h:mm a');
 
+//Define current hour in military hours. 
+//Military hours allow additive counting and avoids am/pm confusion.    
 let now = parseInt(moment().format('kk'));
 
+//Checks each textarea element and updates class.
 $("textarea").each(function () {
     const name = parseInt($(this).attr("name"));
     if (name < now) {
@@ -24,10 +27,7 @@ $("textarea").each(function () {
     }
 });
     
-    
-    
-    
-    
+// Define variables for textarea elements
 const $text9AM = $("#text9AM");
 const $text10AM = $("#text10AM");
 const $text11AM = $("#text11AM");
@@ -38,33 +38,29 @@ const $text3PM = $("#text3PM");
 const $text4PM = $("#text4PM");
 const $text5PM = $("#text5PM");
 
-
+//Define default, black object to be loaded
 const dailyPlans = {'9AM': "", '10AM': "", '11AM': "", '12PM': "", '1PM': "", '2PM': "", '3PM': "", '4PM': "", '5PM': ""};
 
 
 
-
-
-
+//Define key with which to associate planner object in storage
 function createKey() {
     const key = moment().format('MM/DD/YYYY');
     return key;
 };
 
 
-
+//Save to local storage as k:v pair 'date: planObject'
+//Object.keys allows object to be iterated like an array.
 function saveTimes(...args){
    Object.keys(dailyPlans).forEach(key => {
        const $text = $(`#text${key}`);
        dailyPlans[key] = $text.val();
-   })
-    // dailyPlans[9] = $text9AM.val();
-    // dailyPlans[10] = $text10AM.val();
-    // dailyPlans[11] = $text11AM.val();
-    // dailyPlans[12] = $text12PM.val();
+   });
     localStorage.setItem(createKey(), JSON.stringify(dailyPlans));
 };
 
+//All buttons with .saveBtn class call saveTimes function when clicked
 $('.saveBtn').click(saveTimes);
 
 function loadFromStorage(...args){
